c4dcea43353a078c8e5dca03e65c23826528bfd3
Pskalou <pascal.padilla@gmail.com>
2021-04-23 22:06:44 +0200

    son pieces

--- project/game2/scripts/piece.gd
+++ project/game2/scripts/piece.gd
@@ -4,10 +4,19 @@ onready var Princesse = get_tree().get_root().find_node("Princesse",true, false)
 
 onready var GUIScript = get_tree().get_root().find_node("GUI",true, false)
 
+var audio_pieces
+func _ready():
+    audio_pieces= [$AudioControleur/piece1, $AudioControleur/piece2, $AudioControleur/piece3]
+
+
 func _on_Area2D_body_entered(body):
     if body == self or body.is_in_group('ennemis'):
         return
+    var son= audio_pieces[ randi() % len(audio_pieces) ]
+    son.play()
+    self.visible= false
     Princesse.points += 1
     GUIScript.change_valPoints(Princesse.points)
     print(Princesse.points)
+    yield(son, 'finished')
     queue_free()
