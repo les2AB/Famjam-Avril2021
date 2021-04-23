c4880872d8ba7755d46b034f15c8fc9a4daf316b
Pskalou <pascal.padilla@gmail.com>
2021-04-23 21:51:08 +0200

    niveau 1

--- project/game2/scripts/exit.gd
+++ project/game2/scripts/exit.gd
@@ -7,11 +7,14 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_Area2D_body_entered(body):
-	if level < 5:
-		print("change scene")
-		level += 1
-	else :
-		print("game over")
+	var _ignore= get_tree().change_scene("res://arcade/arcade.tscn")
+
+	### En attendant...
+	# if level < 5:
+	# 	print("change scene")
+	# 	level += 1
+	# else :
+	# 	print("game over")
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- project/game2/scripts/piece.gd
+++ project/game2/scripts/piece.gd
@@ -5,6 +5,8 @@ onready var Princesse = get_tree().get_root().find_node("Princesse",true, false)
 onready var GUIScript = get_tree().get_root().find_node("GUI",true, false)
 
 func _on_Area2D_body_entered(body):
+    if body == self or body.is_in_group('ennemis'):
+        return
     Princesse.points += 1
     GUIScript.change_valPoints(Princesse.points)
     print(Princesse.points)
