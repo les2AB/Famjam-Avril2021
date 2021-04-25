26454f717ac7780b681ad1cbae5f05fcba948455
bubobou <bruno.bourgine@gmail.com>
2021-04-25 13:41:15 +0200

    test scene bis

---
+++ project/game2/scripts/game_2_bis.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+onready var world = get_parent()
+
+onready var niveau = preload("res://game2/scenes/Niveaux/Niveau_Bruno.tscn").instance()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_child(niveau)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _input(event):
+	if event is InputEventKey and event.is_pressed():
+		var letter = event.scancode
+		if  letter == KEY_ESCAPE:
+			_leave_game()
+
+
+
+# retour à la salle d'arcade
+func _leave_game() -> void:
+	world._loader_start( 1 )
+	queue_free()
