498331b9f322b9daf7e4a23e0d12bc25c49fa208
bubobou <bruno.bourgine@gmail.com>
2021-04-25 17:06:52 +0200

    disparition p iece

--- project/game2/scripts/Piece.gd
+++ project/game2/scripts/Piece.gd
@@ -1,22 +1,23 @@
 extends AnimatedSprite
 
-onready var Princesse = get_tree().get_root().find_node("Princesse",true, false)
-
+#onready var Princesse = get_tree().get_root().find_node("Princesse",true, false)
+onready var GameNode = get_tree().get_root().find_node("game_2bis",true, false)
 onready var GUIScript = get_tree().get_root().find_node("GUI",true, false)
 
 var audio_pieces
 func _ready():
-    audio_pieces= [$AudioControleur/piece1, $AudioControleur/piece2, $AudioControleur/piece3]
+	audio_pieces= [$AudioControleur/piece1, $AudioControleur/piece2, $AudioControleur/piece3]
 
 
 func _on_Area2D_body_entered(body):
-    if body == self or body.is_in_group('ennemis'):
-        return
-    var son= audio_pieces[ randi() % len(audio_pieces) ]
-    son.play()
-    self.visible= false
-    Princesse.points += 1
-    GUIScript.change_valPoints(Princesse.points)
-    print(Princesse.points)
-    yield(son, 'finished')
-    queue_free()
+	if body == self or body.is_in_group('ennemis'):
+		return
+	var son= audio_pieces[ randi() % len(audio_pieces) ]
+	son.play()
+	queue_free()
+	
+	GameNode.princessePoints += 1
+	GUIScript.change_valPoints(GameNode.princessePoints)
+	print("point :",GameNode.princessePoints)
+	yield(son, 'finished')
+	
