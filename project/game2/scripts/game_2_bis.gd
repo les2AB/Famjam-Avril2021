c5d4f3ae698669cdf31a7109ac69685bdacbee21
Pskalou <pascal.padilla@gmail.com>
2021-04-26 13:36:12 +0200

    ok

--- project/game2/scripts/game_2_bis.gd
+++ project/game2/scripts/game_2_bis.gd
@@ -5,8 +5,6 @@ onready var world = get_parent()
 onready var niveau1 = preload("res://game2/scenes/Niveaux/Niveau_Bruno.tscn")
 
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	GlobalGame2.princessePoints = 0
 	GlobalGame2.princesseVie = 3
@@ -14,14 +12,11 @@ func _ready():
 	GlobalGame2.chargeNiveau()
 
 	
-
-
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		var letter = event.scancode
 		if  letter == KEY_ESCAPE:
 			_leave_game()
-
 
 
 # retour à la salle d'arcade
