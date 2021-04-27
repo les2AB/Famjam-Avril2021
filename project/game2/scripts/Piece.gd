e2a4378d0709d7962d7c7efe69b568f09edd0ae1
Pskalou <pascal.padilla@gmail.com>
2021-04-27 13:15:49 +0200

    audio pièces

--- project/game2/scripts/Piece.gd
+++ project/game2/scripts/Piece.gd
@@ -4,19 +4,23 @@ extends AnimatedSprite
 #onready var GameNode = get_tree().get_root().find_node("game_2bis",true, false)
 
 var audio_pieces
+var GUIScript
 func _ready():
 	audio_pieces= [$AudioControleur/piece1, $AudioControleur/piece2, $AudioControleur/piece3]
+	GUIScript = get_tree().get_root().find_node("GUI_score_vie",true, false)
 
 
 func _on_Area2D_body_entered(body):
-	var GUIScript = get_tree().get_root().find_node("GUI_score_vie",true, false)
 	if body == self or body.is_in_group('ennemis'):
 		return
-	var son= audio_pieces[ randi() % len(audio_pieces) ]
-	son.play()
+	
+	self.visible= false
+	
 	GlobalGame2.princessePoints += 1
 	GUIScript.change_valPoints(GlobalGame2.princessePoints)
-	queue_free()
 	
+	var son= audio_pieces[ randi() % len(audio_pieces) ]
+	son.play()
 	yield(son, 'finished')
+	queue_free()
 	
