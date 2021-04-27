004e143add8f36e7a697463111a112de71f4f308
Pskalou <pascal.padilla@gmail.com>
2021-04-27 11:33:38 +0200

    Ennemis qui s'enfoncent Fixes #11

--- project/game2/scripts/Ennemi.gd
+++ project/game2/scripts/Ennemi.gd
@@ -5,6 +5,7 @@ export (int) var gravity = -981
 
 
 onready var ennemiAudioControl= get_node("EnnemiAudioControl")
+onready var move_collision = get_node("Move_collision")
 
 var velocity= Vector2.ZERO
 
@@ -36,13 +37,13 @@ func _process(delta):
 
 func mourir():
 	velocity.x= 0
+	remove_from_group('ennemis')
 	print ("enemi meurt")
 	ennemiAudioControl.play_scream()
 	$AnimatedSprite.play("meurt")
-	# $Move_collision.set_deferred('disabled', true)
-	$Move_collision.shape.extents= Vector2(0.1,0.001)
-	$Move_collision.position += Vector2(0,9)
-	# $Move_collision.set_deferred('disabled', true)
+	# réduction de la zone de contact et ajustement pour que l'ennemi ne descende pas
+	move_collision.set_deferred('scale', Vector2(1,0))
+	move_collision.set_deferred('position', Vector2(0,13))
 	$Hit_collision/CollisionCorps.set_deferred('disabled', true)
 
 
